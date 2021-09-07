d04dc7c14f4ba01189a9e01f62e0e44817e67411
Sofia <sofiadurkan@gmail.com>
2021-09-07 11:21:11 -0500

    header button added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+import { Fragment } from 'react';
+
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary/>
+            <AvailableMeals/>
+        </Fragment>
+    )
+}
+
+export default Meals
