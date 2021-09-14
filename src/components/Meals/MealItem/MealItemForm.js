11dfa25dea549cf3923293e1fe7fd7eebc48cede
Sofia <sofiadurkan@gmail.com>
2021-09-14 16:59:18 -0500

    card provider added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import {useRef, useState} from 'react'
 import classes from "./MealItemForm.module.css"
 import Input from "../../UI/Input"
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false);
+            return
+
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+
+    };
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount" 
+        <form className={classes.form} onSubmit = {submitHandler}>
+            <Input 
+            ref = {amountInputRef}
+            label="Amount" 
             input={{
                 id:'amount_',
                 type:'number',
@@ -15,6 +36,7 @@ const MealItemForm = (props) => {
                 defaultValue:'1',
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount(1-5)</p>}
         </form>
     )
 }
