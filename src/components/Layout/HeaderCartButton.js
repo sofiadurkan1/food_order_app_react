414f7b1948b6a82f7c1112b805bfef38e14a2b56
Sofia <sofiadurkan@gmail.com>
2021-10-15 10:42:03 -0500

    cart button animation added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,46 @@
-import { useContext } from 'react';
+import { useContext ,useEffect,useState} from 'react';
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context"
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
 
+    const [btnIsHighlighted, setbBnIsHighlighted] = useState(false)
+
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;//destructuring//we want if cart item changed, do bump..
+
 
     const numberOfCartItems = cartCtx.items?.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+
+    const btnClasses = `${classes.button} ${ btnIsHighlighted ?classes.bump : ''}`;
+
+    useEffect(() => { //give bump to button.. everytime new item added
+        if (items.length === 0) {
+            return;
+        }
+        setbBnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setbBnIsHighlighted(false);
+        },300)
+
+
+        return () =>{
+            clearTimeout(timer);
+        };
+
+        
+        
+    }, [items]);
+
+
+
     return (
-       <button className={classes.button} onClick={props.onClick}>
+       <button className={btnClasses} onClick={props.onClick}>
            <span className={classes.icon}>
                <CartIcon/>
            </span>
